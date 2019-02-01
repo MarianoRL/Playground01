59554ee7f379d4fb09eed2e90c736c567e81c7bc
Mariano Ramírez <marianordl98@gmail.com>
2019-01-31 18:04:32 -0600

    primer commit

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var v1 = "Esto es Swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola Mundo! " + v1
+print("El Contenido es: \(cadena)")
+print("El valor de la constante es: \(c1)")
